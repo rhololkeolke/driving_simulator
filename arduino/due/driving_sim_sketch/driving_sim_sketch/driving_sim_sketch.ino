db4f7db450eda000cf1253c9faf09c5983d1f064
Devin Schwab <digidevin@gmail.com>
2014-04-19 14:49:46 -0400

    Added code to read andsave the state of the keyswitch input

--- arduino/due/driving_sim_sketch/driving_sim_sketch/driving_sim_sketch.ino
+++ arduino/due/driving_sim_sketch/driving_sim_sketch/driving_sim_sketch.ino
@@ -1,13 +1,19 @@
 #define ESTOP_PIN 22
+#define KEYSWITCH_PIN 42
+
+bool keyswitch_on = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(ESTOP_PIN, INPUT);
+  pinMode(KEYSWITCH_PIN, INPUT);
   attachInterrupt(ESTOP_PIN, activateEstop, HIGH);
 }
 
 void loop() {
-  Serial.println("running");
+  Serial.print("Keyswitch: ");
+  keyswitch_on = digitalRead(KEYSWITCH_PIN);
+  Serial.println(keyswitch_on);
   delay(500);
 }
 
